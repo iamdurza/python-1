5b4a99a1c6255858d93cdb3c1a7c602264c6a222
Jake Ludington <durzaskillz@gmail.com>
2015-06-27 17:50:15 -0400

    save

---
+++ utils.py
@@ -0,0 +1,26 @@
+"""This my awesome utility kitchen sink. Don't judge me."""
+
+import colors as 
+
+
+
+def ask(question,color=c.red):
+    print(color + question + c.reset)
+    answer = input("> " + c.base3).lower().strip()
+    print(c.reset)
+    return answer
+
+
+
+if __name__ == '__main__':
+    print(c.clear)
+    name = ask("what is your name?")
+    color = ask("What is your favorite color?",c.random_color())
+
+
+
+
+
+
+
+
